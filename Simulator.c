f375b025e3c16f5fded752e0911d5d795d40b707
Parinthorn Saithong <parinthorn.sa@gmail.com>
2016-11-19 22:59:21 +0700

    Refactor const names

--- Simulator.c
+++ Simulator.c
@@ -5,64 +5,66 @@
 long MISS;
 long HIT;
 struct Cache cache[INDEX_SIZE];
+
 int init() {
-	MISS=0;
-	HIT=0;
+	MISS = 0;
+	HIT = 0;
 	int i;
-	for(i=0; i<INDEX_SIZE; i++) {
-		cache[i].valid=0;
-		cache[i].tag=0;
-		cache[i].dirty=0;
+	for(i = 0; i < INDEX_SIZE; i++) {
+		cache[i].valid = 0;
+		cache[i].tag = 0;
+		cache[i].dirty = 0;
 	}
 }
-int calAddr(unsigned long addr,unsigned long *tag,unsigned long *idx,unsigned long *offset) {
+
+int calAddr(unsigned long addr, unsigned long *tag, unsigned long *idx, unsigned long *offset) {
 	unsigned long tmp;
 	int i;
-	*tag=addr>>(OFFSETLEN+INDEXLEN);
-	tmp=0;
-	for(i=0; i<INDEXLEN; i++) {
-		tmp<<=1;
-		tmp|=0x01;
+	*tag = addr >> (OFFSET_LENGTH + INDEX_LENGTH);
+	tmp = 0;
+	for(i = 0; i < INDEX_LENGTH; i++) {
+		tmp <<= 1;
+		tmp |= 0x01;
 	}
-	*idx=addr>>OFFSETLEN & tmp;
-	tmp=0;
-	for(i=0; i<OFFSETLEN; i++) {
-		tmp<<=1;
-		tmp|=0x01;
+	*idx = addr >> OFFSET_LENGTH & tmp;
+	tmp = 0;
+	for(i = 0; i < OFFSET_LENGTH; i++) {
+		tmp <<= 1;
+		tmp |= 0x01;
 	}
-	*offset=addr & tmp;
+	*offset = addr & tmp;
 }
+
 int access(unsigned long addr){
 	unsigned long offset;
 	unsigned long tag;
 	unsigned long idx;
 	int i;
-	int valid=0;
-	calAddr(addr,&tag,&idx,&offset);
-	if(cache[idx].tag==tag && cache[idx].valid) {
+	int valid = 0;
+	calAddr(addr, &tag, &idx, &offset);
+	if(cache[idx].tag == tag && cache[idx].valid) {
 		HIT++;
-	}
-	else{
+	} else {
 		MISS++;
-		cache[idx].valid=1;
+		cache[idx].valid = 1;
 		cache[idx].tag = tag;
 	}
-
 }
-int main(int argc,char *argv[]){
+
+int main(int argc, char *argv[]){
 	FILE *input;
 	char buff[1025];
 	unsigned long myaddr;
-	if (argc<2) {
-		fprintf(stderr,"Please specify address file\n");
+	if (argc < 2) {
+		fprintf(stderr, "Please specify address file\n");
 		exit(-1);
 	}
-	input=fopen(argv[1],"r");
-	//read file
-	while (fgets(&buff[0],1024,input)) {
-		sscanf(buff,"0x%x",&myaddr);
+	input = fopen(argv[1], "r");
+	// Read file
+	while (fgets(&buff[0], 1024, input)) {
+		sscanf(buff, "0x%x", &myaddr);
 		access(myaddr);
 	}
-	printf("HIT:%7d MISS: %7d",HIT,MISS);
+	printf("HIT:%7d MISS: %7d\n", HIT, MISS);
 
 }
--- Simulator.h
+++ Simulator.h
@@ -4,9 +4,9 @@
 #define CACHE_SIZE 1024
 #define INDEX_SIZE 256
 #define BLOCK_SIZE 4
-#define TAGLEN 22
-#define INDEXLEN 8
-#define OFFSETLEN 2
+#define TAG_LENGTH 22
+#define INDEX_LENGTH 8
+#define OFFSET_LENGTH 2
 typedef unsigned char Byte;
 typedef unsigned char bool;
 typedef struct Cache {
