a1ce428989b2c0718613fca3d8e20be42d13ab5f
JThanat <thanat.jat@gmail.com>
2016-11-22 19:04:09 +0700

    implement LRU

--- src/Associativity.cpp
+++ src/Associativity.cpp
@@ -2,6 +2,7 @@
 #include "Cache.h"
 #include "math.h"
 #include <vector>
+#include <cstdio>
 using namespace std;
 #define BYTE_OFFSET 2
 #define STARTING_AGE 0
@@ -58,22 +59,47 @@ void Associativity::access(unsigned long addr) {
         }
         
     } else if (this->replacementAlgo == ReplacementAlgorithm::LEAST_RECENTLY_USED) {
-       
-       
+       bool isHit = false;
+       bool isInvalid = false;
+       unsigned long minWays = 0;
+       for(int way = 0 ; way < nways ; way++) {
+           if(caches[minWays][index]._age <  caches[way][index]._age) minWays = way;
+           if(!caches[way][index].valid) {
+               caches[way][index].tag = tag;
+               caches[way][index].valid = true;
+               caches[way][index]._age = age;
+               isInvalid = true;
+               break;
+           } else if(caches[way][index].tag == tag && caches[way][index].valid) {
+               isHit = true;
+               caches[way][index]._age = age;
+               break;
+           }
+       }
+       if(!isHit && isInvalid) {
+           ++miss;
+       } else if(!isHit) {
+           caches[minWays][index]._age = age;
+           caches[minWays][index].tag = tag;
+           ++miss;
+       } else {
+           ++hit;
+       }
+       ++age;
     } else {
         printf("Unknown Replacement Algorithm\n");
     }
 }
 
-void Associativity::tokenizeAddr(unsigned long addr, unsigned long *tag, unsigned long *idx, unsigned long *byteOffset) {
-    usigned long tmp;
+void Associativity::tokenizeAddr(unsigned long addr, unsigned long *tag, unsigned long *index, unsigned long *byteOffset) {
+    unsigned long tmp;
     *tag = addr >> (this->byteOffsetLength + this->indexLength);
     tmp = 0;
     for(int i = 0 ; i < this->indexLength; i++) {
         tmp <<= 1;
         tmp |= 0x01;
     }
-    *index = addr >> (this->byteOffsetLengths) & tmp;
+    *index = addr >> (this->byteOffsetLength) & tmp;
     tmp = 0;
     for(int i = 0 ; i < this->byteOffsetLength ; i++) {
         tmp <<= 1;
